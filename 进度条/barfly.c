6a76e8ebb169c8262d8eefc014151787d751c92a
chengzhongyuan <1651333184@qq.com>
2025-09-12 15:06:04 +0800

    增加颜色控制

--- 进度条/barfly.c
+++ 进度条/barfly.c
@@ -9,7 +9,8 @@ void Barfly()// 函数的定义
     // 至少循环101次
     while(cnt <= 100)
     {
-        printf("[%-100s][%d%%][%c]\r",bar,cnt,label[cnt%4]);// 这个%100s是预留空间的意思%是一个特殊符号，两个%就可以正常显示
+        printf("\033[32m[%-100s][%d%%][%c]\033[0m\r",bar,cnt,label[cnt%4]);// 这个%100s是预留空间的意思%是一个特殊符号，两个%就可以正常显示
+        // printf("[%-100s][%d%%][%c]\r",bar,cnt,label[cnt%4]);// 这个%100s是预留空间的意思%是一个特殊符号，两个%就可以正常显示
         fflush(stdout);
         bar[cnt++] = STYLE;
         //sleep(1);
