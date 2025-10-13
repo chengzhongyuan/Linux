1b38d09d911fe9d789639b381d0970c91124bf38
chengzhongyuan <1651333184@qq.com>
2025-10-13 16:34:09 +0800

    进程控制_1

---
+++ 16_ProcessControl/fork_num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int cnt = 0;
+
+int main()
+{
+    int ret = fork();
+    while(1)
+    {
+        if(ret<0){
+            printf("fork error! ret = %d",cnt);
+            break;
+        }
+        else if(ret == 0){
+            while(1)
+            {
+                sleep(1);
+            }
+        }
+        cnt++;
+    }
+    return 0;
+}
