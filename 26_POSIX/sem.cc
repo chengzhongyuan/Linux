331914bec3ac0058299bc195ca40d3956ae48d61
chengzhongyuan <1651333184@qq.com>
2025-11-10 09:54:38 +0800

    信号量铺垫

--- 26_POSIX/sem.cc
+++ 26_POSIX/sem.cc
@@ -1,3 +1,50 @@
-// ��Ϣ����
-// �����ڴ�
-// �ź���
+// ��Ϣ����
+// �����ڴ�
+// �ź���
+/*
+��ͨ�ź�
+ 1) SIGHUP       2) SIGINT       3) SIGQUIT      4) SIGILL       5) SIGTRAP
+ 6) SIGABRT      7) SIGBUS       8) SIGFPE       9) SIGKILL     10) SIGUSR1
+11) SIGSEGV     12) SIGUSR2     13) SIGPIPE     14) SIGALRM     15) SIGTERM
+16) SIGSTKFLT   17) SIGCHLD     18) SIGCONT     19) SIGSTOP     20) SIGTSTP
+21) SIGTTIN     22) SIGTTOU     23) SIGURG      24) SIGXCPU     25) SIGXFSZ
+26) SIGVTALRM   27) SIGPROF     28) SIGWINCH    29) SIGIO       30) SIGPWR
+31) SIGSYS      
+
+ʵʱ�ź�
+34) SIGRTMIN    35) SIGRTMIN+1  36) SIGRTMIN+2  37) SIGRTMIN+3
+38) SIGRTMIN+4  39) SIGRTMIN+5  40) SIGRTMIN+6  41) SIGRTMIN+7  42) SIGRTMIN+8
+43) SIGRTMIN+9  44) SIGRTMIN+10 45) SIGRTMIN+11 46) SIGRTMIN+12 47) SIGRTMIN+13
+48) SIGRTMIN+14 49) SIGRTMIN+15 50) SIGRTMAX-14 51) SIGRTMAX-13 52) SIGRTMAX-12
+53) SIGRTMAX-11 54) SIGRTMAX-10 55) SIGRTMAX-9  56) SIGRTMAX-8  57) SIGRTMAX-7
+58) SIGRTMAX-6  59) SIGRTMAX-5  60) SIGRTMAX-4  61) SIGRTMAX-3  62) SIGRTMAX-2
+63) SIGRTMAX-1  64) SIGRTMAX
+
+�����ź��Ǹ����̷��ģ�����Ҫ��ʶ����Ҫ�ж�Ӧ�Ķ��� 
+
+#include <signal.h>
+
+typedef void (*sighandler_t)(int);
+
+sighandler_t signal(int signum, sighandler_t handler);
+*/
+
+#include<iostream>
+#include<unistd.h>
+#include <signal.h>
+using namespace std;
+void handler(int sig)
+{
+    cout<<"我收到了二号信号"<<endl;
+}
+int main()
+{
+    // 仅仅是设置了对二号信号的捕捉方法，并不代表被调用了
+    signal(2, handler); 
+    while(1)
+    {
+        cout<<"I am process:"<<getpid()<<endl;
+        sleep(1);
+    }
+    return 0;
+}
