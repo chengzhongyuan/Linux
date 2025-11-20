89357040c209915b3ec56f9875ef1abc0a5bfb15
chengzhongyuan <1651333184@qq.com>
2025-11-20 11:47:00 +0800

    sigaction

--- 27_signal/mysigaction.cc
+++ 27_signal/mysigaction.cc
@@ -22,6 +22,9 @@ void handler(int signo)
     /* 
     当我们正在递达某一个信号期间，同类信号无法被递达
     系统会把当前信号加入到进程的信号屏蔽字，在完成改动作之后。系统又会解除
+    
+    一般一个信号被接触block之后，会自动进行递达当前屏蔽信号。所以这个信号才会再跑一次
+    信号处理的原则就是串行处理同类信号，不允许递归
     */
 }
 
@@ -31,7 +34,8 @@ int main()
     act.sa_handler = handler;
     act.sa_flags = 0;
     sigemptyset(&act.sa_mask);
-
+    // 当我们正在处理某一种信号时，会自动限制和解开屏蔽，可以顺便屏蔽其他信号
+    sigaddset(&act.sa_mask,3);// 只有在执行二号的时候三号才是会被屏蔽的
     sigaction(SIGINT,&act,&oact);
     // 这个oact信息是会被保存的，可以方便我们去复原
 
