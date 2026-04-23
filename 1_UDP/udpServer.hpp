cb05172b356bbddc7264ab9e3dada25bb5da5550
chengzhongyuan <1651333184@qq.com>
2026-04-23 17:08:51 +0800

    解耦模块化

--- 1_UDP/udpServer.hpp
+++ 1_UDP/udpServer.hpp
@@ -2,15 +2,23 @@
 #define UDP_SERVER_HPP
 
 #include <string>
+#include <functional>
+#include <unordered_map>
+#include <memory>
+#include <array>
 
+// 必须在类声明的上方，先定义好这个类型是什么！
+using ServiceCallback = std::function<std::string(const std::string&)>;
 class UdpServer {
 public:
-    UdpServer(int port);
+    // 构造函数：强迫在创建服务器时，就必须塞给它一个处理逻辑
+    UdpServer(int port, ServiceCallback cb);
     void run();
 
 private:
     int sockfd;
     int port;
+    ServiceCallback callback_; // 保存传进来的业务逻辑
 };
 
 #endif
